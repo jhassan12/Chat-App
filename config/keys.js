48e80c66564220ef0b47276ed0bb526c55aaf210
Jahanzeb Hassan <imtiazwaraich@Imtiazs-MBP.fios-router.home>
2020-07-24 23:58:54 -0400

    deleted keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-CONNECTION_STRING = 'mongodb+srv://jhassan1:' + encodeURIComponent('appleboy124') + '@cluster0-0ltv6.mongodb.net/test?retryWrites=true&w=majority';
-SESSION_SECRET = 'secret';
-
-module.exports = {
-	connectionString : CONNECTION_STRING,
-	sessionSecret : SESSION_SECRET
-};
